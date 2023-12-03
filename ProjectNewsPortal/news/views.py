ec88f5e5a6d7f47d810f08069fd1876de9b8f41c
Aleksandr-hub-cyber <aleksandrsilver42@gmail.com>
2023-12-03 16:13:33 +0800

    Update views.py

--- ProjectNewsPortal/news/views.py
+++ ProjectNewsPortal/news/views.py
@@ -1,36 +1,36 @@
-from django.shortcuts import render
-from datetime import datetime
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import *
+from datetime import datetime
 
-# Create your views here.
 
-
-class PostsList(ListView):
+class PostList(ListView):
 
     model = Post
-    ordering = "-created_at"
 
-    template_name = "news.html"
-    context_object_name = "posts"
+    ordering = '-time_create'
 
-    # def get_context_data(self, **kwargs):
-    #
-    #     context = super().get_context_data(**kwargs)
-    #     context["time_now"] = datetime.utcnow()
-    #
-    #     return context
+    template_name = 'news.html'
 
-    Post.objects.all()
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context['time_now'] = datetime.utcnow()
+
+        context['next_sale'] = None
+        return context
 
 
 class PostDetail(DetailView):
 
     model = Post
-    template_name = "news_detail.html"
-    context_object_name = "post"
 
-    def get_context_data(self, **kwargs):
+    template_name = 'news_detail.html'
+
+    context_object_name = 'post'
+
 
         context = super().get_context_data(**kwargs)
         context["time_now"] = datetime.utcnow()
